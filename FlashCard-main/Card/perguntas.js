6f78d415f96c164379cc4e910ea46a4973c9f0a1
diegodamke <diego.belgrovicz@escola.pr.gov.br>
2025-08-06 12:13:02 -0300

    Add files via upload

---
+++ FlashCard-main/Card/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'CONHECIMENTOS GERAIS',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison'
+)
+
+criaCartao(
+    'Programação',
+    'Variáveis',
+    'Variáveis são espaços de memória nomeados que armazenam valores. Elas podem mudar de valor ao longo da execução do programa. Por exemplo, uma variável pode armazenar o número de cliques em um botão ou o nome de um usuário. '
+)
+
+criaCartao(
+    'Programação',
+    'Tipos de Dados',
+    'Tipos de dados definem a natureza dos valores que uma variável pode armazenar. Alguns exemplos comuns são: Inteiro (int): Números inteiros (1, 2, -5, 0). Ponto flutuante (float): Números com casas decimais (3.14, -2.5). Texto (string): Sequências de caracteres ("Olá", "Programação" Booleano (bool): Verdadeiro (True) ou falso (False). '
+)
+
+criaCartao(
+    'Programação',
+    'Operadores',
+    'Operadores são símbolos que realizam operações em valores. Alguns exemplos comuns são: Aritméticos: +, -, *, / (adição, subtração, multiplicação, divisão). Relacionais: == (igual), != (diferente), > (maior que), < (menor que). Lógicos: && (E), || (OU), ! (NÃO). '
+)
+
+criaCartao(
+    'Programação',
+    'Condicionais',
+    'Condicionais permitem que o programa tome decisões com base em condições. As estruturas condicionais mais comuns são if (se), else (senão) e elif (senão se). Exemplo: se a variável "idade" for maior ou igual a 18, o programa pode imprimir "Maior de idade"'
+)
+
+criaCartao(
+    'Programação',
+    'Estruturas de Repetição',
+    'Estruturas de Repetição: Estruturas de repetição (loops) permitem que um bloco de código seja executado várias vezes. Os loops mais comuns são for (para) e while (enquanto). Exemplo: um loop for pode percorrer todos os itens de uma lista, realizando alguma operação em cada um. '
+)
