266c375a732fb443bdd6736aa6cf032b27071fc8
diegodamke <diego.belgrovicz@escola.pr.gov.br>
2025-08-07 10:47:25 -0300

    Add files via upload

---
+++ Card/perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'CIÊNCIAS',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison'
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'HISTÓRIA',
+    'Que grande evento histórico aconteceu em 1822 no Brasil?',
+    'Independência do Brasil.'
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'Em que país de localiza o Taj Mahal?    ',
+    'Índia'
+)
+
+criaCartao(
+    'QUIMICA',
+    'Qual destas substâncias faz parte da composição do vidro?',
+    'Areia'
+)
+criaCartao(
+    'BIOLOGIA',
+    'Quantos ossos temos no nosso corpo?',
+    '206'
+)
